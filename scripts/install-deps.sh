e790d0a18a618de4fcd6284df0a4ad9def5db7c2
Warren E. Downs <vwdowns@gmail.com>
2020-02-05 17:37:46 -0900

    Restarting development

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,5 +4,5 @@ printf "Checking dependencies...\r"
 which node >/dev/null || sudo apt install node
 which npm  >/dev/null || sudo apt install npm
 which pnpm >/dev/null || sudo npm install -g pnpm
-pnpm install colors console-ten watchr websocket better-sqlite3 node-ipc command-line-args errlop editions
+pnpm install colors console-ten watchr websocket better-sqlite3 node-ipc command-line-args errlop editions gm
 printf "\r                            \r"
